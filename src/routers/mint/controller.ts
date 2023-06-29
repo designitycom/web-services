b55557c05532c7d5ddab434ef482c60de49bb635
amirsharifico <amirsharifico@gmail.com>
2023-06-29 13:25:27 +0000

    issue#29 2an3of4 completed

--- src/routers/mint/controller.ts
+++ src/routers/mint/controller.ts
@@ -2,7 +2,7 @@ import controller from "../controller";
 import { Request, Response } from "express";
 import fs from "fs";
 import { Connection, Client, ConnectionOptions } from "@temporalio/client";
-import { createMintWF, updateMintWF } from "../../workflows/mint/workflows";
+import { createMintWF, getMintAddress, getUpdatedMintAddress, updateMintWF } from "../../workflows/mint/workflows";
 import * as activities from "./../../workflows/mint/activities";
 import { plainToClass, plainToClassFromExist } from "class-transformer";
 import { MintDTO } from "../../models/mintDto";
@@ -88,6 +88,52 @@ class MintController extends controller {
     worker.run();
     res.send("worker run");
   };
-}
+//-------------
+  getMintedTokenData = async (req:Request, res:Response)=>{
+    const workFlowId = req.params.workFlowId
+    const connection = await Connection.connect(temporalConnConfig);
+    const client = new Client({
+      connection,
+      namespace: process.env.TEMPORAL_NAMESPACE || "default",
+    });
+    console.log(workFlowId);
+    const handle = client.workflow.getHandle(workFlowId);
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log("mint controller after temporal connection")
+    const val = await handle.query(getMintAddress);
+    console.log(val);
+
+    await handle.result();
+    console.log("complete");
+
+
+    console.log(`Workflow Started `);
+    this.myResponse(res, 200, val, "set workflow");
+
+  }
+
+  getUpdatedMintTokenData = async (req:Request, res:Response)=>{
+    const workFlowId = req.params.workFlowId
+    const connection = await Connection.connect(temporalConnConfig);
+    const client = new Client({
+      connection,
+      namespace: process.env.TEMPORAL_NAMESPACE || "default",
+    });
+    console.log(workFlowId);
+    const handle = client.workflow.getHandle(workFlowId);
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log("mint controller after temporal connection")
+    const val = await handle.query(getUpdatedMintAddress);
+    console.log(val);
+
+    await handle.result();
+    console.log("complete");
+
+
+    console.log(`Workflow Started `);
+    this.myResponse(res, 200, val, "set workflow"); 
+  }
+
+}// end of MintController
 
 export default new MintController();
