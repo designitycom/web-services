a2cb8a47fc36b315b94b90d984555198d710f481
AmirSaber Sharifi <inbox@saberistic.com>
2023-08-18 07:47:43 -0600

    update scoreAccountDTO to include two new

--- src/workflows/mint/activities.ts
+++ src/workflows/mint/activities.ts
@@ -29,7 +29,9 @@ export async function scoreAccountDTO(scoreAccount: any) {
     name: scoreAccount.name as string,
     level: scoreAccount.levels as Array<string>,
     scores: scoreAccount.scores as Array<string>,
-    scores_sum: scoreAccount.scoresSum as Array<string>
+    scores_sum: scoreAccount.scoresSum as Array<string>,
+    reviews_received: Math.max(...scoreAccount.reviewsRecieved),
+    reviews_sent: scoreAccount.reviewsSent
   }
 }
 
