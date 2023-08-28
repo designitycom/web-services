bb66488514826c932d860825ef84108b967e0a4c
AmirSaber Sharifi <inbox@saberistic.com>
2023-08-28 11:48:48 -0400

    use next function

--- src/routers/airtable/controller.ts
+++ src/routers/airtable/controller.ts
@@ -1,31 +1,32 @@
 import controller from "../controller";
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { plainToClass } from "class-transformer";
 import { AirTableDTO } from "../../models/airTableDto";
-import {
-  processPendingScoresWF,
-} from "../../workflows/airtable/workflows";
+import { processPendingScoresWF } from "../../workflows/airtable/workflows";
 import { createTemporalClient } from "../../services/temporal";
 
 class AirTableController extends controller {
-  processPendingScores = async (req: Request, res: Response) => {
+  processPendingScores = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
     const airTableDto = plainToClass(AirTableDTO, req.body);
     console.log(`started ${airTableDto.wfId} `);
     try {
-    const client = await createTemporalClient();
-    const workFlowId = "airtable-" + airTableDto.wfId;
-    client.workflow.start(processPendingScoresWF, {
-      args: [airTableDto],
-      taskQueue: "airtable",
-      workflowId: workFlowId
-    });
+      const client = await createTemporalClient();
+      const workFlowId = "airtable-" + airTableDto.wfId;
+      client.workflow.start(processPendingScoresWF, {
+        args: [airTableDto],
+        taskQueue: "airtable",
+        workflowId: workFlowId,
+      });
+      this.myResponse(res, 200, {}, "set workflow");
     } catch (err) {
-	    console.log(err);
+      console.log(err);
+      next(err);
     }
-    this.myResponse(res, 200, {}, "set workflow");
   };
-
-
 }
 
 export default new AirTableController();
