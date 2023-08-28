9d6abd10576aad48152617d0c40ad5783725988f
AmirSaber Sharifi <inbox@saberistic.com>
2023-08-28 11:49:05 -0400

    fix wait period to 4 months

--- src/routers/test/controller.ts
+++ src/routers/test/controller.ts
@@ -69,7 +69,7 @@ class TestController extends controller {
   growth = async (req: Request, res: Response) => {
     const g = getGrowthService();
     try {
-      await g.createOrganization("Designity", [4, 1, 1, 1, 1, 2, 1, 1, 1, 1], [2], [[25, 50, 75], [25, 75]], "https://public.designity.software", 15, 7890000);
+      await g.createOrganization("Designity", [4, 1, 1, 1, 1, 2, 1, 1, 1, 1], [2], [[25, 50, 75], [25, 75]], "https://public.designity.software", 15, 10520000);
     } catch (err) {
       console.error(err);
     }
