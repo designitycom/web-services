b7f8bff29d4028deb2a8690105fe13e62bebbaa4
Wasif Shah <wasif1024@gmail.com>
2024-01-11 02:48:18 +0500

    latest

--- src/services/solana.ts
+++ src/services/solana.ts
@@ -1,55 +1,53 @@
-import * as web3 from "@solana/web3.js"
-import * as fs from "fs"
-import * as path from "path"
-import dotenv from "dotenv"
+import * as web3 from "@solana/web3.js";
+import * as fs from "fs";
+import * as path from "path";
+import dotenv from "dotenv";
 import { Keypair, Connection as conn } from "@solana/web3.js";
 import * as jose from "jose";
-import { Metaplex, PublicKey, bundlrStorage, keypairIdentity } from "@metaplex-foundation/js";
+import {
+  Metaplex,
+  PublicKey,
+  bundlrStorage,
+  keypairIdentity,
+} from "@metaplex-foundation/js";
 import { BN } from "bn.js";
 import { GrowthService } from "./growth";
-dotenv.config()
+dotenv.config();
 //test git
-export async function airdrop(
-  connection: web3.Connection,
-  sol: number
-) {
-  const secret = JSON.parse(process.env.PRIVATE_KEY ?? "") as number[]
-  const secretKey = Uint8Array.from(secret)
-  const keypairFromSecretKey = web3.Keypair.fromSecretKey(secretKey)
+export async function airdrop(connection: web3.Connection, sol: number) {
+  const secret = JSON.parse(process.env.PRIVATE_KEY ?? "") as number[];
+  const secretKey = Uint8Array.from(secret);
+  const keypairFromSecretKey = web3.Keypair.fromSecretKey(secretKey);
   const airdropSignature = await connection.requestAirdrop(
     keypairFromSecretKey.publicKey,
     sol * web3.LAMPORTS_PER_SOL
-  )
-  const latestBlockHash = await connection.getLatestBlockhash()
+  );
+  const latestBlockHash = await connection.getLatestBlockhash();
 
   await connection.confirmTransaction({
     blockhash: latestBlockHash.blockhash,
     lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
     signature: airdropSignature,
-  })
-  const balance = await connection.getBalance(keypairFromSecretKey.publicKey)
-  return balance
+  });
+  const balance = await connection.getBalance(keypairFromSecretKey.publicKey);
+  return balance;
 }
-export async function getBalance(
-  connection: web3.Connection
-) {
-  const secret = JSON.parse(process.env.PRIVATE_KEY ?? "") as number[]
-  const secretKey = Uint8Array.from(secret)
-  const keypairFromSecretKey = web3.Keypair.fromSecretKey(secretKey)
-  const balance = await connection.getBalance(keypairFromSecretKey.publicKey)
-  return balance
-
+export async function getBalance(connection: web3.Connection) {
+  const secret = JSON.parse(process.env.PRIVATE_KEY ?? "") as number[];
+  const secretKey = Uint8Array.from(secret);
+  const keypairFromSecretKey = web3.Keypair.fromSecretKey(secretKey);
+  const balance = await connection.getBalance(keypairFromSecretKey.publicKey);
+  return balance;
 }
 export async function createKeypair(
   connection: web3.Connection,
   secretKeyAddress: string
 ): Promise<web3.Keypair> {
+  const secret = JSON.parse(secretKeyAddress ?? "") as number[];
+  const secretKey = Uint8Array.from(secret);
+  const keypairFromSecretKey = web3.Keypair.fromSecretKey(secretKey);
 
-  const secret = JSON.parse(secretKeyAddress ?? "") as number[]
-  const secretKey = Uint8Array.from(secret)
-  const keypairFromSecretKey = web3.Keypair.fromSecretKey(secretKey)
-
-  return keypairFromSecretKey
+  return keypairFromSecretKey;
 }
 export function getGrowthService() {
   const c = getConnection();
@@ -59,36 +57,36 @@ export function getGrowthService() {
     )
   );
   let authority = Keypair.fromSecretKey(decodedAuthorityKey);
-  
-
 
   return new GrowthService(c, getKeyPair("AUTHORITY"), getKeyPair("MINT"));
 }
 const fromHexString = (hexString: string) =>
-  Uint8Array.from(hexString!.match(/.{1,2}/g)!.map((byte) => parseInt(byte, 16)));
-export function initializeKeypair(
-  connection: web3.Connection
-): web3.Keypair {
+  Uint8Array.from(
+    hexString!.match(/.{1,2}/g)!.map((byte) => parseInt(byte, 16))
+  );
+export function initializeKeypair(connection: web3.Connection): web3.Keypair {
   if (!process.env.PRIVATE_KEY) {
-    console.log("Creating .env file")
-    const signer = web3.Keypair.generate()
-    fs.writeFileSync(".env", `PRIVATE_KEY=[${signer.secretKey.toString()}]`)
-    airdropSolIfNeeded(signer, connection)
+    console.log("Creating .env file");
+    const signer = web3.Keypair.generate();
+    fs.writeFileSync(".env", `PRIVATE_KEY=[${signer.secretKey.toString()}]`);
+    airdropSolIfNeeded(signer, connection);
 
-    return signer
+    return signer;
   }
 
-  const secret = JSON.parse(process.env.PRIVATE_KEY ?? "") as number[]
-  const secretKey = Uint8Array.from(secret)
-  const keypairFromSecretKey = web3.Keypair.fromSecretKey(secretKey)
+  const secret = JSON.parse(process.env.PRIVATE_KEY ?? "") as number[];
+  const secretKey = Uint8Array.from(secret);
+  const keypairFromSecretKey = web3.Keypair.fromSecretKey(secretKey);
 
   // airdropSolIfNeeded(keypairFromSecretKey, connection)
-  return keypairFromSecretKey
+  return keypairFromSecretKey;
 }
 export function getKeyPair(keyName: "AUTHORITY" | "MINT") {
   const decodedMintKey = new Uint8Array(
     JSON.parse(
-      fs.readFileSync(process.env[`${keyName}_KEY_ADDRESS`] as string).toString()
+      fs
+        .readFileSync(process.env[`${keyName}_KEY_ADDRESS`] as string)
+        .toString()
     )
   );
   return Keypair.fromSecretKey(decodedMintKey);
@@ -117,10 +115,10 @@ export function makeSimpleMetaplex() {
   return metaplex;
 }
 export function publicKeyFromBn(bn: string) {
-  const bigNumber = new BN(bn, 16)
+  const bigNumber = new BN(bn, 16);
   const decoded = { _bn: bigNumber };
   return new PublicKey(decoded);
-};
+}
 
 export async function getWalletPublicKeyFromIdToken(idToken: string) {
   const jwks = jose.createRemoteJWKSet(
@@ -129,7 +127,9 @@ export async function getWalletPublicKeyFromIdToken(idToken: string) {
   const jwtDecoded = await jose.jwtVerify(idToken, jwks, {
     algorithms: ["ES256"],
   });
-  return publicKeyFromBn((jwtDecoded.payload as any).wallets[0].public_key).toBase58();
+  return publicKeyFromBn(
+    (jwtDecoded.payload as any).wallets[0].public_key
+  ).toBase58();
 }
 
 export async function getEmailFromIdToken(idToken: string) {
@@ -142,38 +142,36 @@ export async function getEmailFromIdToken(idToken: string) {
   return (jwtDecoded.payload as any).email;
 }
 
-
 async function airdropSolIfNeeded(
   signer: web3.Keypair,
   connection: web3.Connection
 ) {
-  const balance = await connection.getBalance(signer.publicKey)
-  console.log("Current balance is", balance / web3.LAMPORTS_PER_SOL)
+  const balance = await connection.getBalance(signer.publicKey);
+  console.log("Current balance is", balance / web3.LAMPORTS_PER_SOL);
 
   if (balance < web3.LAMPORTS_PER_SOL) {
-    console.log("Airdropping 1 SOL...")
-    console.log(web3.LAMPORTS_PER_SOL + " sol")
-    console.log("public key" + signer.publicKey.toString())
+    console.log("Airdropping 1 SOL...");
+    console.log(web3.LAMPORTS_PER_SOL + " sol");
+    console.log("public key" + signer.publicKey.toString());
     try {
-
       const airdropSignature = await connection.requestAirdrop(
         signer.publicKey,
         web3.LAMPORTS_PER_SOL
-      )
-      console.log("Airdropping sended")
+      );
+      console.log("Airdropping sended");
 
-      const latestBlockHash = await connection.getLatestBlockhash()
+      const latestBlockHash = await connection.getLatestBlockhash();
 
       await connection.confirmTransaction({
         blockhash: latestBlockHash.blockhash,
         lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
         signature: airdropSignature,
-      })
+      });
 
-      const newBalance = await connection.getBalance(signer.publicKey)
-      console.log("New balance is", newBalance / web3.LAMPORTS_PER_SOL)
+      const newBalance = await connection.getBalance(signer.publicKey);
+      console.log("New balance is", newBalance / web3.LAMPORTS_PER_SOL);
     } catch (er) {
-      console.log('Error Here: ' + er)
+      console.log("Error Here: " + er);
     }
   }
 }
--- src/workflows/airtable/activities.ts
+++ src/workflows/airtable/activities.ts
@@ -2,50 +2,56 @@ import { FieldSet, Record } from "airtable";
 import { getConnectionAirTable } from "../../services/airTable";
 
 export interface ICreativesScoresAirtable extends FieldSet {
-  'Email': string;
-  'Hard Skill (Calculated)': number;
-  'Creativity_design_sense': number;
-  'Accountability': number;
-  'Attention_to_detail': number;
-  'Communication_presentation': number;
-  'Feedback_listening': number;
-  'Leadership_guidance': number;
-  'Management_organisation': number;
-  'Strategic_thinking': number;
-  'Team_collaboration': number;
-  'Creative Director Email': string;
-  'Creative Director Name': string;
-  'Wallet Address': string[];
-  'Creatives': string[];
-  'Submitted On': string;
-  'Score Eligible?': string[];
+  Email: string;
+  "Hard Skill (Calculated)": number;
+  Creativity_design_sense: number;
+  Accountability: number;
+  Attention_to_detail: number;
+  Communication_presentation: number;
+  Feedback_listening: number;
+  Leadership_guidance: number;
+  Management_organisation: number;
+  Strategic_thinking: number;
+  Team_collaboration: number;
+  "Creative Director Email": string;
+  "Creative Director Name": string;
+  "Wallet Address": string[];
+  Creatives: string[];
+  "Submitted On": string;
+  "Score Eligible?": string[];
 }
 
 export interface ISoftrCreativesUser extends FieldSet {
-  "Email": string;
-  'Token Address': string;
-  'Wallet Address': string;
-  'Magic Link': string;
-  'Start Date'?: string[];
-  'Name': string;
-  'Status': number;
-  'Level': number;
-  'Profile image'?: string[];
-  'Dashboard - Backend iFrame'?: string[];
+  Email: string;
+  "Token Address": string;
+  "Wallet Address": string;
+  "Magic Link": string;
+  "Start Date"?: string[];
+  Name: string;
+  Status: number;
+  Level: number;
+  "Profile image"?: string[];
+  "Dashboard - Backend iFrame"?: string[];
 }
 
 export interface ICreativesMaster extends FieldSet {
   "Personal Email Address": string;
 }
 
-
-export async function findRecordWithEmail(email: string): Promise<Record<ISoftrCreativesUser> | undefined> {
+export async function findRecordWithEmail(
+  email: string
+): Promise<Record<ISoftrCreativesUser> | undefined> {
   console.log("find by email");
-  const base = (await getConnectionAirTable()).base('appxprwH6zsJbTFyM');
-  const findRecords = await base<ISoftrCreativesUser>('Users').select({
-    view: "Softr / Smart Contract Users for Growth Master",
-    filterByFormula: `{Email} = '${email}'`,
-  }).all();
+  // const base = (await getConnectionAirTable()).base("appxprwH6zsJbTFyM");
+  const base = (await getConnectionAirTable()).base(
+    `${process.env.AIRTABLE_BASE}`
+  );
+  const findRecords = await base<ISoftrCreativesUser>("Creatives Softr Users")
+    .select({
+      view: "Softr / Smart Contract Users",
+      filterByFormula: `{Email} = '${email}'`,
+    })
+    .all();
 
   if (findRecords.length < 1) {
     return undefined;
@@ -55,47 +61,61 @@ export async function findRecordWithEmail(email: string): Promise<Record<ISoftrC
 }
 
 export async function getCreativeWallet(recId: string) {
-  const base = (await getConnectionAirTable()).base("appBwrlSCBQDC9UCV");
+  // const base = (await getConnectionAirTable()).base("appBwrlSCBQDC9UCV");
+  const base = (await getConnectionAirTable()).base(
+    `${process.env.AIRTABLE_BASE}`
+  );
+
   const result = await base<ICreativesMaster>("Creatives").find(recId);
-  const record = await findRecordWithEmail(result.fields["Personal Email Address"]);
+  const record = await findRecordWithEmail(
+    result.fields["Personal Email Address"]
+  );
   return record?.fields["Wallet Address"];
 }
 
 export async function updateScoreTX(recId: string, tx: string) {
-  const base = (await getConnectionAirTable()).base("appBwrlSCBQDC9UCV");
+  // const base = (await getConnectionAirTable()).base("appBwrlSCBQDC9UCV");
+  const base = (await getConnectionAirTable()).base(
+    `${process.env.AIRTABLE_BASE}`
+  );
 
   await base("🧑‍🎨 Creatives Scores").update(recId, {
     "Transaction ID": tx,
   });
 }
 
+export async function updateSoftrCreativeUsers(
+  recordId: string,
+  record: ISoftrCreativesUser
+) {
+  // const base = (await getConnectionAirTable()).base("appxprwH6zsJbTFyM");
+  const base = (await getConnectionAirTable()).base(
+    `${process.env.AIRTABLE_BASE}`
+  );
 
-
-export async function updateSoftrCreativeUsers(recordId: string, record: ISoftrCreativesUser) {
-  const base = (await getConnectionAirTable()).base('appxprwH6zsJbTFyM');
   delete record["Start Date"];
   delete record["Profile image"];
   delete record["Dashboard - Backend iFrame"];
   delete record["ACTIVE?"];
-  return await base("Creatives Softr Users").update(
-    [
-      {
-        id: recordId,
-        fields: record,
-      },
-    ],);
+  return await base("Creatives Softr Users").update([
+    {
+      id: recordId,
+      fields: record,
+    },
+  ]);
 }
 
-
-
 export async function getPendingScores() {
-  const base = (await getConnectionAirTable()).base('appBwrlSCBQDC9UCV');
+  const base = (await getConnectionAirTable()).base("appBwrlSCBQDC9UCV");
   try {
-    return await base<ICreativesScoresAirtable>("🧑‍🎨 Creatives Scores").select({
-      view: "Grid view",
-      filterByFormula: "AND({Transaction ID} = '', {Wallet Address}, {Submitted On})",
-      sort: [ {field: "Submitted On", direction: "asc"}]
-    }).all();
+    return await base<ICreativesScoresAirtable>("🧑‍🎨 Creatives Scores")
+      .select({
+        view: "Grid view",
+        filterByFormula:
+          "AND({Transaction ID} = '', {Wallet Address}, {Submitted On})",
+        sort: [{ field: "Submitted On", direction: "asc" }],
+      })
+      .all();
   } catch (err) {
     throw err;
   }
