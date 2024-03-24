4934aa6fa15c3018389394296b8720b63647778f
Wasif Shah <wasif1024@gmail.com>
2024-03-24 23:44:42 +0500

    removed cd code

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,9 +1,7 @@
 import express from "express";
 import testRouter from "./test";
 import mintRouter from "./mint";
-import mintCdRouter from "./mint_cd";
 import airTableRouter from "./airtable";
-import airTableCdRouter from "./airtable_cd";
 import handleError from "../middlewares/error";
 const router = express.Router();
 
@@ -16,7 +14,5 @@ router.use("/health", (req, res) => {
 router.use("/test", testRouter); //site.con/api/test
 router.use("/mint", mintRouter); //site.con/api/mint
 router.use("/airtable", airTableRouter); //site.con/api/airtable
-router.use("/airtablecd", airTableCdRouter); //site.con/api/airtable
-router.use("/mintcd", mintCdRouter); //site.con/api/airtable
 router.use(handleError);
 export default router;
